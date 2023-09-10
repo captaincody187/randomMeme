589a755587a2c3b0153fc314a372e8b377652ee0
Cody <simoncody121@gmail.com>
2023-09-09 23:05:11 -0500

    second commit

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ const memesAhoy = [
   'pics/south-park_gay-fish.png',
   'pics/shockedPikachu.jpg',
   'pics/CaptureFunnyOne.PNG',
+  'pics/one.jpg',
+  'pics/two.jpg',
+  'pics/three.jpg',
+  'pics/four.jpg',
+  'pics/five.jpg',
+  'pics/six.jpg',
+  'pics/seven.jpg',
+  'pics/eight.jpg',
+  'pics/nine.jpg',
+  'pics/ten.jpg',
+  'pics/eleven.jpg',
+  'pics/twelve.jpg',
+  'pics/thirteen.jpg',
+  'pics/fourteen.jpg',
+  'pics/fifteen.jpg',
+  'pics/sixteen.jpg',
+  'pics/seventeen.jpg',
+  'pics/eighteen.jpg',
+  'pics/nineteen.jpg',
+  'pics/twenty.jpg',
 ];
 
 memeM = () => {
@@ -16,8 +36,14 @@ memeM = () => {
   let img = document.createElement('img');
   img.src = x;
   img.width = 300;
-  img.height = 200;
+  img.height = 300;
 
   // Append the img element to the memeZone div
   memeZone.appendChild(img);
+};
+
+// Clear the meme from the screen
+noMas = () => {
+  let memeZone = document.getElementById('memeZone');
+  memeZone.innerHTML = '';
 };
